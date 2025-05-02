44bdc8fed03621f0e1d2278b9215fcd255aa93ed
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-05-02 11:01:44 -0600

    add protected route and clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,28 @@
-import { defineMiddleware } from "astro:middleware";
+// import { defineMiddleware } from "astro:middleware";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
-export const onRequest = defineMiddleware((context, next) => {
-    const lang = context.request.headers.get("Accept-language")
-    const [langCode] = lang?.split(',') || []
-    console.log({ langCode })
+// Custom middleware
+// export const onRequest = defineMiddleware((context, next) => {
+//     const lang = context.request.headers.get("Accept-language")
+//     const [langCode] = lang?.split(',') || []
+//     console.log({ langCode })
 
-    if (langCode === 'en-US') {
-        context.locals.title = 'English App'
-        // return context.redirect('/en-US/')
-    } else {
-        context.locals.title = 'Otra app de Retos'
+//     if (langCode === 'en-US') {
+//         context.locals.title = 'English App'
+//         // return context.redirect('/en-US/')
+//     } else {
+//         context.locals.title = 'Otra app de Retos'
+//     }
+//     return next();
+// })
+
+const isProtectedRoute = createRouteMatcher([
+    '/retos(.*)'
+])
+export const onRequest = clerkMiddleware((auth, context) => {
+    const { userId, redirectToSignIn } = auth();
+
+    if (isProtectedRoute(context.request) && !userId) {
+        return redirectToSignIn()
     }
-    return next();
-})
+});
